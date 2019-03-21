ed71ff8d29d1e4d749bfe4a0c35ebb8e557796be
ValeriiaMur <41483072+ValeriiaMur@users.noreply.github.com>
2019-03-20 17:40:41 -0700

    Add files via upload

---
+++ solutions/d02/ex00/sortStone.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sortStone.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmuradia <vmuradia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/20 17:25:19 by vmuradia          #+#    #+#             */
+/*   Updated: 2019/03/20 17:38:28 by vmuradia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+
+void sortStones(struct s_stone **stone)
+{
+
+}
+
+struct s_stone* sortedMerge(struct s_stone *a, struct s_stone *b)
+{
+
+}
+
+void split(struct s_stone *source, struct s_stone **frontRef, struct s_stone **back_ref)
+{
+
+}
+
+/* sorts the linked list by changing next pointers (not data) */
+void MergeSort(struct s_stone **headRef) 
+{
+    
+}
