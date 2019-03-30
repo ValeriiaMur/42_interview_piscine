d99c178f423240a41f6ff08f37c10a31977eeaf3
ValeriiaMur <41483072+ValeriiaMur@users.noreply.github.com>
2019-03-29 19:28:36 -0700

    Add files via upload

---
+++ solutions/d05/ex00/possibleSlices.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   possibleSlices.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: valeriiamuradian <valeriiamuradian@stud    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/29 18:14:35 by valeriiamur       #+#    #+#             */
+/*   Updated: 2019/03/29 22:25:40 by valeriiamur      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header.h"
+#include <stdio.h>
+
+void printPossibleSlices(int pizzaSize)
+{
+    struct s_array *arr = arrayInit();
+    recursive(aar, pizzaSize)
+}
+
+// 3
+// 2 1
+// 1 1 1
+
